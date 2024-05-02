d579779e047ec25d9998b457693baba33652aad8
berkandemircii <102293172+berkandemircii@users.noreply.github.com>
2024-05-02 21:10:09 +0300

    Update EmployeeHolder.java

--- SE322_V2/src/main/java/org/example/EmployeeHolder.java
+++ SE322_V2/src/main/java/org/example/EmployeeHolder.java
@@ -9,15 +9,25 @@ public class EmployeeHolder {
         arrayList.add(emp);
     }
 
+    public static void RemoveFromArrayList(Employee emp){
+        arrayList.remove(emp);
+    }
+
+
+
     public static void showEmployees(){
         for (int i = 0; i< arrayList.size();i++){
             Employee tempEmp = arrayList.get(i);
-            System.out.println(i + "-" + tempEmp.getId() + " : " + tempEmp.getName());
+            System.out.println(i + " - ID=" + tempEmp.getId() + " : " + tempEmp.getName());
         }
     }
 
     public static Employee getEmployee(int index){
-        return arrayList.get(index);
+        if (index >= 0 && index < arrayList.size()) {
+            return arrayList.get(index);
+        } else {
+            return null; // Return null if index is out of bounds
+        }
     }
 
     public static int getSize(){
