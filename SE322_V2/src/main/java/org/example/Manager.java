987b8b7fcd980a41b0c6f6f553a5f9c865238ef4
berkandemircii <102293172+berkandemircii@users.noreply.github.com>
2024-05-02 21:11:02 +0300

    Update Manager.java

--- SE322_V2/src/main/java/org/example/Manager.java
+++ SE322_V2/src/main/java/org/example/Manager.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-class Manager extends Employee{    // DivideToSubtasks methodu yok
+class Manager extends Employee{
     HashMap<Worker,Task> WaitingForApprove = new HashMap<>();
     Scanner scanner = new Scanner(System.in);
 
@@ -14,17 +14,22 @@ class Manager extends Employee{    // DivideToSubtasks methodu yok
     }
 
     // bunun çalışması için bizim employeeleri tuttuğumuz bir genel arrayden, input olarak verilen employee objesini silmemiz lazım
-    public Employee FireEmployee(Employee input){
-        return null;
+    public Employee FireEmployee(int index){
+        Employee deletedEmployee = EmployeeHolder.getEmployee(index);
+        EmployeeHolder.RemoveFromArrayList(EmployeeHolder.getEmployee(index));
+        return deletedEmployee;
     }
 
     //bunun icinde yaratınca genel bir employee arrayi varsa içine atabilir ya da return ettigi yerde eklenebilir
     public Employee CreateEmployee(String EmployeeID, String name, String ContactDetail, String jobRole){
         return new Employee(EmployeeID, name, ContactDetail, jobRole);
     }
+    public Task CreateTask(String ID, String desc, int point){
+        return new Task(ID, desc, point);
+    }
 
     //just for the change the employee attributes
-    public void updateEmployee(Employee employee){
+    public  void updateEmployee(Employee employee){
         int temp_input =0;
         System.out.println();
         System.out.println("To change employee ID press 1 \nTo change employee name press 2 \nTo change employee contact detail press 3 \nTo change employee job role press 4 \n");
@@ -69,8 +74,11 @@ class Manager extends Employee{    // DivideToSubtasks methodu yok
             System.out.println("Is Finished (Write \"Yes\" or \"No\")");
             String string = scanner.nextLine();
 
+
             if (string.toLowerCase().replace(" ","").equals("yes")){
-                WaitingForApprove.remove(set.getKey(),set.getValue());
+                set.getValue().setStatus(Task.TaskStatus.DONE);
+                WaitingForApprove.remove(set.getKey(),set.getValue());// Burada task'ın done olduğunu aşağıda belirtmemiz lazım. Sonradan bitmiş işleri görmek istersek orada kullanılır.
+
             }
             else if (string.toLowerCase().replace(" ","").equals("no")){
                 GiveTask(set.getKey(),set.getValue());
@@ -79,9 +87,6 @@ class Manager extends Employee{    // DivideToSubtasks methodu yok
                 System.out.println("Wrong input given and task skipped");
             }
         }
-
-
-
     }
 
     public void GiveTask(Employee employee, Task task){
@@ -139,6 +144,13 @@ class Manager extends Employee{    // DivideToSubtasks methodu yok
             }
         }
         return choice2.equals("Y")? true : false;
+    }
+
+    // bu method Sinem'in attığı java dosyası ile gelmişti ondan silmedim. Sizle konuşup silmek istedim
+    // Method to separate a task to subtasks
+    //Employee den buraya taşındı çünkü bunu sadece manager yapabiliyor.
+    public void separateTaskToSubtasks(Task task, ArrayList<Task> subTasks) {
+        task.getSubTasks().addAll(subTasks);
     }
 
     public boolean chooseUpdate(){
