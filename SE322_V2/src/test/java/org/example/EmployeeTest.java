87fa6072cb42684baf7fe0b2c012463bff743407
SinemYesil <105049945+SinemYesil@users.noreply.github.com>
2024-05-12 18:19:13 +0300

    Update EmployeeTest.java

--- SE322_V2/src/test/java/org/example/EmployeeTest.java
+++ SE322_V2/src/test/java/org/example/EmployeeTest.java
@@ -107,7 +107,7 @@ class EmployeeTest {
     @Test
     void separateTaskPositive() { // PASSED
         // Positive test case, seperating tasks into subtasks are tested
-        Manager manager = new Manager("1", "Manager", "manager@office.com", "Manager");
+        Manager manager = new Manager("1", "Manager", "manager@office.com", "Manager",8,17);
         Task task = new Task("1", "Complete project report", 5);
         Task subtask1 = new Task("1.1", "Gather project data", 2);
         Task subtask2 = new Task("1.2", "Analyze project requirements", 3);
@@ -122,7 +122,7 @@ class EmployeeTest {
     @Test
     void separateTaskNegative() { // PASSED
         // Negative test case, trying to separate tasks when subtasks list is null is tested
-        Manager manager = new Manager("1", "Manager", "manager@office.com", "Manager");
+        Manager manager = new Manager("1", "Manager", "manager@office.com", "Manager",8,17);
         Task task = new Task("1", "Complete project report", 5);
         List<Task> nullSubtasks = null;
         assertThrows(NullPointerException.class, () -> manager.separateTaskToSubtasks(task, (ArrayList<Task>) nullSubtasks));
