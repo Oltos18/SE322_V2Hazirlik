8d1b8711a943e3bd6020e407067c339b7cac8303
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-05-28 22:29:51 +0300

    düzenlemeler yapıldı

--- SE322_V2/src/test/java/org/example/IsOkTest.java
+++ SE322_V2/src/test/java/org/example/IsOkTest.java
@@ -26,5 +26,4 @@ class IsOkTest {
         assertNotEquals(false, result);
     }
 
-
 }
--- SE322_V2/src/test/java/org/example/ReportTest.java
+++ SE322_V2/src/test/java/org/example/ReportTest.java
@@ -1,32 +1,63 @@
 package org.example;
+
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Calendar;
-import java.util.Scanner;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ReportTest {
+public class ReportTest {
 
     @Test
-    void setReportTimerPositive() {  // NEEDS TO BE FIXED
-        Report report = new Report();
-        report.setReportTimer();
+    public void testReportWithCompletedTasks() {
+        Employee emp1 = new Employee("1", "John Black", "john@office.com", "123Abc", "Software Developer");
+        Employee emp2 = new Employee("2", "Jenna Bailey", "jenna@office.com", "123Cde", "Software Developer");
 
-        Calendar expectedCalendar = Calendar.getInstance();
-        expectedCalendar.add(Calendar.DATE, 1);
-        assertEquals(expectedCalendar.getTime(), report.calendar.getTime());
+        Task task1 = new Task("1", "Complete project report", 5);
+        task1.setEndTime();
+        Task task2 = new Task("2", "Gather project data", 2);
+        task2.setEndTime();
+
+        emp1.CompleteTask(task1);
+        emp2.CompleteTask(task2);
+
+        EmployeeHolder.AddToArraylist(emp1);
+        EmployeeHolder.AddToArraylist(emp2);
+
+        Report report = new Report();
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        report.task.run();
+
+        System.setOut(System.out);
+
+        String expectedOutput = "Employee John Black get 5 points\nEmployee Jenna Bailey get 2 points\n";
+        assertEquals(expectedOutput, outContent.toString().trim());
     }
 
     @Test
-    void setReportTimerNegative() { // NEEDS TO BE FIXED
+    public void testReportWithoutCompletedTasks() {
+        Employee emp1 = new Employee("1", "John Doe", "password", "contact", "Developer");
+        Employee emp2 = new Employee("2", "Jane Smith", "password", "contact", "Designer");
+
+        EmployeeHolder.AddToArraylist(emp1);
+        EmployeeHolder.AddToArraylist(emp2);
+
         Report report = new Report();
-        report.setReportTimer();
 
-        Calendar expectedCalendar = Calendar.getInstance();
-        assertNotEquals(expectedCalendar.getTime(), report.calendar.getTime());
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        report.task.run();
+
+        System.setOut(System.out);
+
+        String expectedOutput = "Employee John Doe get 0 points\nEmployee Jane Smith get 0 points\n";
+        assertEquals(expectedOutput, outContent.toString());
     }
-
 }
