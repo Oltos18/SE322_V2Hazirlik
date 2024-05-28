ec60ff35f72c6b41045565a4fb7685cba3c4e531
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-05-28 20:06:06 +0300

    yeni testler eklendi, devam edilecek

--- SE322_V2/src/test/java/org/example/TrendsTest.java
+++ SE322_V2/src/test/java/org/example/TrendsTest.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.example.Employee;
+import org.example.Manager;
+import org.example.Task;
+import org.example.EmployeeHolder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TrendsTest {
+
 }
+
