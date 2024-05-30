4b060d83ef481737a1cc80e78201d238a829655a
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-05-30 21:53:26 +0300

    suite case yeniden yazıldı fakat hala versiyon problemi var

--- SE322_V2/src/test/java/org/example/SuiteTestAll.java
+++ SE322_V2/src/test/java/org/example/SuiteTestAll.java
@@ -1,40 +1,40 @@
-//package org.example;
-//
-//import org.junit.platform.suite.api.SelectClasses;
-//import org.junit.platform.suite.api.Suite;
-//
-//@Suite
-//@SelectClasses({
-//        AddingCompletedTasksTest.class,
-//        AddToArrayListTest.class,
-//        AddToWaitingForApproveTest.class,
-//        AssignWorkHourTest.class,
-//        ChooseUpdateTest.class,
-//        CompleteTaskTest.class,
-//        CreateEmployeeTest.class,
-//        EmployeeConstructorTest.class,
-//        FireEmployeeTest.class,
-//        GetIndexEdgeTest.class,
-//        GetIndexInputTest.class,
-//        GetIndexValidInputTest.class,
-//        GiveTaskTest.class,
-//        IsOkTest.class,
-//        LogConstructorTest.class,
-//        LogHistoryTest.class,
-//        ManagerConstructorTest.class,
-//        PasswordCharacterTest.class,
-//        PasswordValidatorTest.class,
-//        ReportTest.class,
-//        SaveTasksTest.class,
-//        SeperateTaskTest.class,
-//        ShowUnassignedTest.class,
-//        TaskConstructorTest.class,
-//        UpdateTaskTest.class,
-//        ValidPasswordLengthTest.class,
-//        ViewAssignedTasksTest.class,
-//        ViewEmployeeTest.class,
-//        ViewUnassignedTasksTest.class,
-//        WorkerConstructorTest.class
-//})
-//public class SuiteTestAll {
-//}
+package org.example;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AddingCompletedTasksTest.class,
+        AddToArrayListTest.class,
+        AddToWaitingForApproveTest.class,
+        AssignWorkHourTest.class,
+        ChooseUpdateTest.class,
+        CompleteTaskTest.class,
+        CreateEmployeeTest.class,
+        EmployeeConstructorTest.class,
+        FireEmployeeTest.class,
+        GetIndexEdgeTest.class,
+        GetIndexInputTest.class,
+        GetIndexValidInputTest.class,
+        GiveTaskTest.class,
+        IsOkTest.class,
+        LogConstructorTest.class,
+        LogHistoryTest.class,
+        ManagerConstructorTest.class,
+        PasswordCharacterTest.class,
+        PasswordValidatorTest.class,
+        ReportTest.class,
+        SaveTasksTest.class,
+        SeperateTaskTest.class,
+        ShowUnassignedTest.class,
+        TaskConstructorTest.class,
+        UpdateTaskTest.class,
+        ValidPasswordLengthTest.class,
+        ViewAssignedTasksTest.class,
+        ViewEmployeeTest.class,
+        ViewUnassignedTasksTest.class,
+        WorkerConstructorTest.class
+})public class SuiteTestAll {
+}
+
+
